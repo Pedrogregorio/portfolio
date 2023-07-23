8f3516875c16c8f876f9abb908ce09b96e9d05ed
Pedro Gregorio <pedrogregorio@jera.com.br>
2023-07-23 05:43:33 -0400

    add footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
 function toggleMenu() {
+  const screenWidth = window.screen.width
+
+  if(screenWidth >= 990) return;
+
   const menu = document.querySelector(".navigation");
   const menuIcon = document.querySelector("header button");
 
